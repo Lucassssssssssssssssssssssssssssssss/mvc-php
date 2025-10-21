62185a1cbb90849b2d7f79a9b36b9a8ad4d9d7d3
Lucassssssssssssssssssssssssssssssss <lucasmidiere1@gmail.com>
2025-10-21 09:35:02 +0200

    Créez un routeur

---
+++ Code/blog/src/controllers/post.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('src/model.php');
+
+function post(string $identifier)
+{
+    $post = getPost($identifier);
+    $comments = getComments($identifier);
+
+    require('templates/post.php');
+}
+
+$post = getPost($identifier);
+$comments = getComments($identifier);
+
+require('templates/post.php');
