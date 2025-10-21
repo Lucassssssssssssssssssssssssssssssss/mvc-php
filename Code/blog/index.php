a5d00360bda5c6d7c31f538e72f82ce02a5cab9f
Lucassssssssssssssssssssssssssssssss <lucasmidiere1@gmail.com>
2025-10-21 09:47:36 +0200

    Ajoutez des commentaires

--- Code/blog/index.php
+++ Code/blog/index.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once('src/controllers/add_comment.php');
 require_once('src/controllers/homepage.php');
 require_once('src/controllers/post.php');
 
@@ -11,7 +12,17 @@ if (isset($_GET['action']) && $_GET['action'] !== '') {
             post($identifier);
         } else {
             echo 'Erreur : aucun identifiant de billet envoyé';
+
+            die;
+        }
+    } elseif ($_GET['action'] === 'addComment') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            $identifier = $_GET['id'];
             
+            addComment($identifier, $_POST);
+        } else {
+            echo 'Erreur : aucun identifiant de billet envoyé';
+
             die;
         }
     } else {
--- Code/blog/src/controllers/post.php
+++ Code/blog/src/controllers/post.php
@@ -1,14 +1,16 @@
 <?php
 
 require_once('src/model.php');
+require_once('src/model/comment.php');
 
 function post(string $identifier)
 {
     $post = getPost($identifier);
     $comments = getComments($identifier);
-
+    
     require('templates/post.php');
 }
+
 
 $post = getPost($identifier);
 $comments = getComments($identifier);
